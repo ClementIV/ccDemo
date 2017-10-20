6136d38d9a8efed7b72f95d26683e0abb37ad32d
ClementIV <fyqnankai@163.com>
2017-10-20 10:44:54 +0800

    创建demo ，开始学习RESTFUL API

---
+++ backend/moudles/api/Module.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace backend\moudles\api;
+
+/**
+ * api module definition class
+ */
+class Module extends \yii\base\Module
+{
+    /**
+     * @inheritdoc
+     */
+    public $controllerNamespace = 'backend\moudles\api\controllers';
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
---
+++ backend/moudles/api/controllers/UersController.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace backend\moudles\api\controllers;
+use yii\rest\ActiveController;
+class UersController extends ActiveController
+{
+    public $modelClass = 'common\models\User';    
+
+}
