9d9a567ab874cc908be3134dac34f0d7c481d6cc
ClementIV <fyqnankai@163.com>
2017-10-31 18:39:21 +0800

    修改跨域访问问题

--- backend/modules/api/controllers/CourseController.php
+++ backend/modules/api/controllers/CourseController.php
@@ -13,38 +13,65 @@ class CourseController extends ActiveController
 
     public function behaviors()
     {
-        return [
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'index'  => ['get'],
-                    'view'   => ['get','options'],
-                    'create' => ['post'],
-                ]
-            ],
-            'corsFilter' => [
-                'class' => Cors::className(),
-                'cors'=>[
-                    'Access-Control-Request-Method' =>['*'],
-                    'Access-Control-Resquest-Header' => ['*'],
-                    'Access-Control-Request-Credentials' => true,
-                    // Allow OPTIONS caching
+        $behaviors = parent::behaviors();
 
-               'Access-Control-Max-Age' => 3600,
-
-               // Allow the X-Pagination-Current-Page header to be exposed to the browser.
-
-               'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
-                ]
-            ],
-            'contentNegotiator'=>[
-                'class' =>ContentNegotiator::className(),
-                'formats' => [
-                    'application/json' =>Response::FORMAT_JSON
-                ]
-            ]
-
+        // remove authentication filter
+        $auth = $behaviors['authenticator'];
+        unset($behaviors['authenticator']);
+        $behaviors['corsFilter'] = [
+       'class' => \yii\filters\Cors::className(),
         ];
+        $behaviors['verbs'] = [
+            'class' => VerbFilter::className(),
+            'actions' => [
+                'index'  => ['get'],
+                'view'   => ['get','options'],
+                'create' => ['post'],
+            ]
+        ];
+       // re-add authentication filter
+       $behaviors['authenticator'] = $auth;
+       // avoid authentication on CORS-pre-flight requests (HTTP OPTIONS method)
+       $behaviors['authenticator']['except'] = ['options'];
+       $behaviors['contentNegotiator']=[
+           'class' =>ContentNegotiator::className(),
+           'formats' => [
+               'application/json' =>Response::FORMAT_JSON
+           ]
+       ];
+       return $behaviors;
+        // return [
+        //     'verbs' => [
+        //         'class' => VerbFilter::className(),
+        //         'actions' => [
+        //             'index'  => ['get'],
+        //             'view'   => ['get','options'],
+        //             'create' => ['post'],
+        //         ]
+        //     ],
+        //     'corsFilter' => [
+        //         'class' => Cors::className(),
+        //     //     'cors'=>[
+        //     //         'Access-Control-Request-Method' =>['*'],
+        //     //         'Access-Control-Resquest-Header' => ['*'],
+        //     //         'Access-Control-Request-Credentials' => true,
+        //     //         // Allow OPTIONS caching
+        //        //
+        //     //    'Access-Control-Max-Age' => 3600,
+        //        //
+        //     //    // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+        //        //
+        //     //    'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
+        //     //     ]
+        //     ],
+        //     'contentNegotiator'=>[
+        //         'class' =>ContentNegotiator::className(),
+        //         'formats' => [
+        //             'application/json' =>Response::FORMAT_JSON
+        //         ]
+        //     ]
+        //
+        // ];
     }
 }
 ?>
